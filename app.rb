30c7fed1db3664d90d0b8c28dd85f6316474a423
Néstor Álvarez Díaz <nad.9205@gmail.com>
2014-10-26 12:00:39 +0000

    ignore y identificadores de apps

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ require 'omniauth-facebook'
 
 use OmniAuth::Builder do
   config = YAML.load_file 'config/config.yml'
-  provider :google_oauth2, config['identifier'], config['secret']
+  provider :google_oauth2, config['identifier_google'], config['secret_goole']
   provider :github, config['identifier_github'], config['secret_github']
   provider :facebook, config['identifier_facebook'], config['secret_facebook']
 end
